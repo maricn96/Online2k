35f0ef2301f947fe8a3d626c5320491126ee2ae3
Nikola Maric <maricnikola752@gmail.com>
2020-01-27 00:01:50 +0100

    Authorization error

---
+++ auth-server/src/main/java/com/auth/authserver/config/UserDetails.java
@@ -0,0 +1,42 @@
+package com.auth.authserver.config;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.auth.authserver.model.User;
+import com.auth.authserver.repository.UserRepository;
+
+@Service
+public class UserDetails implements UserDetailsService {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public org.springframework.security.core.userdetails.UserDetails loadUserByUsername(String email)
+			throws UsernameNotFoundException {
+
+		User user = userRepository.findByEmail(email);
+		
+		if(user == null) {
+			throw new UsernameNotFoundException("User with email " + email + " not found!");
+		}
+		
+		List<GrantedAuthority> grantedAuthorities = new ArrayList<GrantedAuthority>();
+		
+		grantedAuthorities.add(new SimpleGrantedAuthority("KORISNIK")); //posle dodati admina (valjda)
+		grantedAuthorities.add(new SimpleGrantedAuthority("ADMIN"));
+		
+		return new org.springframework.security.core.userdetails.User(email, user.getPassword(), true, true, true, true, grantedAuthorities);
+	}
+
+	
+	
+}
---
+++ auth-server/src/main/java/com/auth/authserver/exception/CustomException.java
@@ -0,0 +1,24 @@
+package com.auth.authserver.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class CustomException extends RuntimeException {
+	
+	private final String message;
+	private final HttpStatus status;
+
+	public CustomException(String message, HttpStatus status) {
+		this.message = message;
+		this.status = status;
+	}
+	
+	@Override
+	public String getMessage() {
+		return message;
+	}
+	
+	public HttpStatus getHttpStatus() {
+		return status;
+	}
+	
+}
